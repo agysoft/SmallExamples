2a2f6a364d1c4b0567bd80f7e5c5b19f8cc3d2de
Nagy László <laszlo.nagy@capsys.hu>
2022-12-09 13:21:34 +0100

    StringListProba, private.xml

---
+++ src/text/StringListProba.java
@@ -0,0 +1,37 @@
+/*
+
+ */
+package text;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ *
+ * @author laszlo.nagy
+ */
+public class StringListProba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        List<String> stringList = new ArrayList<String>();
+        
+        stringList.add("VERIFIED");
+        System.out.println(stringList.toString());
+        System.out.println(stringList.toString().substring(1, stringList.toString().length() -1));
+        stringList.add("VERIFIED");
+        System.out.println(stringList.toString());
+        System.out.println(stringList.toString().substring(1, stringList.toString().length() -1));
+        
+        
+        
+    }
+    
+
+}
